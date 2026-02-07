de517979a1db4b5aadccd9f077004e4c0b370cd9
gab <ptdm321123@gmail.com>
2026-02-07 11:41:44 -0300

    fix: execution class not disabled

--- Program.cs
+++ Program.cs
@@ -4,13 +4,8 @@
     {
         static void Main(string[] args)
         {
-            // Execution e = new Execution();
-            // e.Start(args);
-
-            ListTasks list = new ListTasks();
-            list.AddTask("teste");
-            list.AddTask("testando");
-            list.ShowFullList();
+            Execution e = new Execution();
+            e.Start(args);
         }
     }
 }
